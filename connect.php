f80b1ea5a13eb327c0a5aac964b1a16cbca94ecc
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-16 12:26:39 -0400

    Created html error message with php script

--- connect.php
+++ connect.php
@@ -8,8 +8,6 @@ $con = mysqli_connect($HOSTNAME, $USERNAME, $PASSWORD, $DATABASE);
 
 if (!$con) {
     die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connection successful";
 }
 
 ?>
--- sign.php
+++ sign.php
@@ -1,4 +1,6 @@
 <?php
+$success=0;
+$user=0;
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     include 'connect.php';
@@ -11,12 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     if($result){
         $num=mysqli_num_rows($result);
         if($num>0){
-            echo "User already exist";
+            // echo "User already exist";
+            $user=1;
         }else{
             $sql = "INSERT INTO registration (username, password) VALUES ('$username', '$password')";
             $result = mysqli_query($con, $sql);
                 if ($result) {
-                        echo 'Signup successful';
+                        // echo 'Signup successful';
+                        $success=1;
                     } else {
                         die(mysqli_error($con));
                     }
@@ -37,6 +41,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
+
+    <?php
+        if($user){
+            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>Holy guacamole!</strong> User already exists
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+          </div>';
+        }
+    ?>
+
     <h1 class="text-center">Sign up page</h1>
     <div class="container mb-5">
         <form class="mb-4" action="sign.php" method="post">
