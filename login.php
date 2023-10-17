fa4f1797d37d5ecdb7705054e9d14149f594beab
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-17 10:18:36 -0400

    Created login page framework. Wording edits to sign.php

--- login.php
+++ login.php
@@ -37,34 +37,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Signup page</title>
+    <title>Login page</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
 
-    <!-- Success Message -->
-    <?php
-        if($success){
-            echo '<div class="alert alert-success alert-dismissible fade show" role="alert">
-            <strong>Success </strong> You are successfully signed up.
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
-        }
-    ?>
-
-    <!-- Error Message -->
-    <?php
-        if($user){
-            echo '<div class="alert alert-danger alert-dismissible fade show" role="alert">
-            <strong>Holy guacamole!</strong> User already exists
-            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-          </div>';
-        }
-    ?>
-
-    <h1 class="text-center">Sign up page</h1>
+    <h1 class="text-center">Login to our website</h1>
     <div class="container mb-5">
-        <form class="mb-4" action="sign.php" method="post">
+        <form class="mb-4" action="login.php" method="post">
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Name</label>
                 <input type="text" class="form-control" placeholder="Enter your username" name="username">
@@ -73,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 <label for="exampleInputPassword1" class="form-label">Password</label>
                 <input type="password" class="form-control" placeholder="Enter your password" name="password">
             </div>
-            <button type="submit" class="btn btn-primary w-100">Sign up</button>
+            <button type="submit" class="btn btn-primary w-100">Login</button>
         </form>
     </div>
     
