42afd9182dfea44f606c89cf40f21bce0b57a756
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-17 12:18:51 -0400

    Added bootstrap html edits to logout hyperlink. Created logout page

--- home.php
+++ home.php
@@ -19,7 +19,7 @@ if(!isset($_SESSION['username'])){
     </h1>
 
     <div class="contianer">
-        <a href="logout.php">Logout</a>
+        <a href="logout.php" class="btn btn-primary mt-5">Logout</a>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
