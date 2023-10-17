7222b4e9e82b83153c955041b1ad825200641d15
Kent Coleman <coleman.a.kent@gmail.com>
2023-10-17 12:16:47 -0400

    Created logout hyperlink on homepage

--- home.php
+++ home.php
@@ -14,8 +14,14 @@ if(!isset($_SESSION['username'])){
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Welcome!</h1>
-    <?php echo $_SESSION['username'];?>
+    <h1 class="text-center text-warning mt-5">Welcome
+        <?php echo $_SESSION['username'];?>
+    </h1>
+
+    <div class="contianer">
+        <a href="logout.php">Logout</a>
+    </div>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
   </body>
 </html>
